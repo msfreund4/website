e3f8c2571ff2656c091187f38e6ec002bba72ec5
msfreund4 <msfreund4@gmail.com>
2026-05-19 17:38:27 +0000

    Deploying to gh-pages from @ msfreund4/website@ee219e6816533ee2a271d9d1ee0d4c05c6535d3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,13 +489,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=TqBiQxgAAAAJ", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/website/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
