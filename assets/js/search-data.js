df7d0231adef87f28bd024514ed7f6a249e1ed1c
msfreund4 <msfreund4@gmail.com>
2026-05-19 16:59:24 +0000

    Deploying to gh-pages from @ msfreund4/website@90b6e273c5e961fc14710a81b87e3ce4d0b3d652 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,7 +472,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%69%63%68%61%65%6C.%66%72%65%75%6E%64@%64%61%6C.%63%61", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -494,13 +494,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
